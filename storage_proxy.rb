713436e7cbdd42ec850c3cf7b225d6a12b468d5f
Randall Floyd <rdfloyd@indiana.edu>
2017-09-07 14:52:13 -0400

    Adding file locater service

--- storage_proxy.rb
+++ storage_proxy.rb
@@ -1,24 +1,44 @@
 class StorageProxy < Sinatra::Base
 
   require 'rufus-scheduler'
+  require './locater_service.rb'
 
   configure :development do
     register Sinatra::Reloader
   end
 
+  locater = LocaterService.new
+
+  # This scheduler's job is to move files from queue to the cache to simulate slow file retrieval.
+  # A file is put into the queue when staged, but it's just a renamed copy of a stub file.
+  # The
   scheduler = Rufus::Scheduler.new
   scheduler.every '15s' do
     puts 'Checking file request queue'
-    # TODO Run a queue management task from here to move files
-    # Maybe a rake task?
-    # system 'rake queue_mgr'
+    # TODO Run a queue management method from here to move files found in queue to cache
   end
 
   get '/' do
     "Some default response goes here"
   end
 
-  get '/some_endpoint/:id' do
+  get '/status/:id' do
+    locater.status(params['id'])
+  end
+
+  get '/stage/:id' do
     "Response for some_endpoint/#{params['id']} goes here"
+  end
+
+  get '/unstage/:id' do
+    "Response for some_endpoint/#{params['id']} goes here"
+  end
+
+  get '/fixity/:id' do
+    "Response for some_endpoint/#{params['id']} goes here"
+  end
+
+  get '/some_other_endpoint/:id' do
+    "Response for some_other_endpoint/#{params['id']} goes here"
   end
 end
