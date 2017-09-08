29549f8875a6175da86e6f9c965d0d6a72c0dcb3
Randall Floyd <rdfloyd@indiana.edu>
2017-09-08 11:46:44 -0400

    Collapsing into a single class design to benefit from automatic Sinatra reloading ability.

--- storage_proxy.rb
+++ storage_proxy.rb
@@ -1,48 +1,73 @@
 class StorageProxy < Sinatra::Base
 
   require 'rufus-scheduler'
-  require './locater_service.rb'
-  require './storage_manager.rb'
 
   configure :development do
     register Sinatra::Reloader
   end
 
-  locater = LocaterService.new
-  storage_mgr = StorageManager.new
-
   # This scheduler's job is to move files from ./storage/queue to ./storage/cache to simulate slow file retrieval.
   # A file is put into the queue when staged - it's just a renamed copy of a stub file from ./storage/archive.
   scheduler = Rufus::Scheduler.new
-  scheduler.every '15s' do
-    puts 'Checking file request queue'
-    # Move all files found in queue to cache
-    storage_mgr.cache_files
+  scheduler.every '10s' do
+    # Move all files found in the queue to the cache
+    move_to_cache
   end
+
+  # Endpoint definitions
 
   get '/' do
     "Some default response goes here"
   end
 
   get '/status/:id' do
-    locater.status(params['id'])
+    locate_file(params['id'])
   end
 
   get '/stage/:id' do
-    storage_mgr.stage(params['id'])
-    "Response for some_endpoint/#{params['id']} goes here"
+    stage(params['id'])
   end
 
   get '/unstage/:id' do
-    storage_mgr.unstage(params['id'])
-    "Response for some_endpoint/#{params['id']} goes here"
+    unstage(params['id'])
   end
 
   get '/fixity/:id' do
     # TODO
   end
 
-  get '/some_other_endpoint/:id' do
-    "Response for some_other_endpoint/#{params['id']} goes here"
+  get '/some_other_example_endpoint/:id' do
+    "Response for some_other_example_endpoint/#{params['id']} goes here"
+  end
+
+  private
+
+  def locate_file(identifier)
+    # TODO
+    # Check for a string in identifier (like foo) mocking a file unknown condition, return error status.
+    # Else check cache in ./storage/cache, return staged status if exists.
+    # Else check queue in ./storage/queue, return staging status if exists.
+    # Else, return a status indicating it isn't staged but is a valid file that can be.
+    [response: "Some response status for #{identifier} goes here"].to_json
+  end
+
+  def stage(identifier)
+    # TODO
+    # Copy ./storage/archive/stub to ./storage/queue/identifier
+    [response: "Some response indicating stage request received for #{identifier} goes here"].to_json
+  end
+
+  def self.move_to_cache
+    puts 'Checking staging queue for files'
+    # Sleep so that we don't move new files as soon as they were created - guarantees an asynchronous wait
+    sleep 10
+    # TODO
+    # Move ./storage/queue/* to ./storage/cache
+  end
+
+  def unstage(identifier)
+    # TODO
+    # Remove ./storage/cache/identifier
+    [response: "Some response indicating unstage request received for #{identifier} goes here"].to_json
   end
 end
